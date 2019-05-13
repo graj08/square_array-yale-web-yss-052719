ad4ae1bb09ca3ead0c2230624e90e0f0cfddb581
gorgeous-karma-7335 <graj08@gmail.com>
2019-05-13 10:58:14 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   squared_array = Array.new # your code here
   array.each do |location|
-squared_array << array[location]+1
+squared_array << array[location]
   end
   squared_array
 end
